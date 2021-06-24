1d80301b31d794faea16fef1268087cddd57baa0
Matheus Ramalho <theuramalho@gmail.com>
2021-06-23 21:45:38 -0300

    Aula 3 - criaçao de sala e entrar em sala

---
+++ src/pages/Home.tsx
@@ -0,0 +1,74 @@
+import { FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+import { AuthPage } from '../components/AuthPage';
+import { AuthMain } from '../components/AuthMain';
+import { Aside } from '../components/Aside';
+import { Button } from '../components/Button';
+
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+export function Home() {
+    const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        history.push('rooms/new'); // Redireciona pra tela de criar a sala
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists');
+            return
+        }
+
+        history.push(`/rooms/${roomCode}`);
+    }
+
+    return (
+        <AuthPage>
+            <Aside />
+
+            <AuthMain>
+                <div className="main__content">
+                    <img src={logoImg} alt="" />
+
+                    <Button className="button__sec" onClick={handleCreateRoom}>
+                        <img src={googleIconImg} alt="" />
+                        Crie sua sala com o Google
+                    </Button>
+
+                    <div className="main__separator"> Ou entre em uma sala </div>
+
+                    <form onSubmit={handleJoinRoom}>
+                        <fieldset>
+                            <legend hidden> Código da sala </legend>
+                            <input
+                                type="text"
+                                placeholder="Informe o código da sala"
+                                onChange={event => setRoomCode(event.target.value)}
+                            />
+                            <Button type="submit"> Entrar na sala </Button>
+                        </fieldset>
+                    </form>
+                </div>
+            </AuthMain>
+        </AuthPage>
+    );
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,69 @@
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+import { AuthPage } from '../components/AuthPage';
+import { AuthMain } from '../components/AuthMain';
+import { Aside } from '../components/Aside';
+import { Button } from '../components/Button';
+
+import logoImg from '../assets/images/logo.svg';
+
+export function NewRoom() {
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        //trim - remove os espaços tanto da direita quanto da esquerda.
+        if (newRoom.trim() === '') {
+            return
+        }
+
+        // Aqui começa a mexer com o firebase.
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({ //push - joga informação para dentro de rooms.
+            title: newRoom,
+            authorId: user?.id,
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
+
+    return (
+        <AuthPage>
+            <Aside />
+
+            <AuthMain>
+                <div className="main__content">
+                    <img src={logoImg} alt="" />
+                    <h2> Criar uma nova sala </h2>
+
+                    <form onSubmit={handleCreateRoom}>
+                        <fieldset>
+                            <legend hidden> Código da sala </legend>
+                            <input
+                                type="text"
+                                placeholder="Nome da sala"
+                                onChange={event => setNewRoom(event.target.value)}
+                                value={newRoom}
+                            />
+                            <Button type="submit"> Criar sala </Button>
+                        </fieldset>
+                    </form>
+
+                    <p> Quer entrar numa sala existente? <Link to="/"> clique aqui </Link> </p>
+                </div>
+            </AuthMain>
+        </AuthPage>
+    )
+}
+
+function userAuth(): { user: any; } {
+    throw new Error('Function not implemented.');
+}
